1e1de07b4020cb9e20bd5228ac963607cbf5626c
Jordan Georgiev Georgiev <jordashiro@gmail.com>
2013-01-18 01:18:33 +0200

    Initial commit

---
+++ src/endjavaapp/EndJavaApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package endjavaapp;
+
+/**
+ *
+ * @author jordashiro
+ */
+public class EndJavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
